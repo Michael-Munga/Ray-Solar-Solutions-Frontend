54efa392ca3600f03d2093709ba77687df2bc3b1
Michael Munga <micknyalle@gmail.com>
2025-07-22 12:40:20 +0300

    create the hero section and its content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/CustomerComponents/Hero";
 import Navbar from "./components/CustomerComponents/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero/>
     </div>
   );
 }
